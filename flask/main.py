871551cbbdda24e256a61cd680ef2064fe0a1337
Caleb James Smith <caleb_smith2@baylor.edu>
2020-11-12 10:08:32 -0700

    setup page to upload images

--- flask/main.py
+++ flask/main.py
@@ -1,16 +1,27 @@
 # main.py
-from flask import Flask
+import uuid
+import numpy as np
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+from flask import Flask, render_template, request, redirect, url_for
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 
+# image classifier from keras
+model = ResNet50(weights='imagenet')
+
+# configure location to save images
+photos = UploadSet(name='photos', extensions=IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, upload_sets=photos)
+
+# get: loading website
+# post: uploading to website
+
 # default url
-@app.route('/')
-def hello_world():
-    return 'Hello and welcome!'
-
-# testing images
-@app.route('/images/')
-def images():
-    return 'Where are all the pictures?'
-
-
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        pass
+    return render_template('upload.html')
