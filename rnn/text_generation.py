3b4d469f5f987c27bbd50b760758111b50cde106
Caleb James Smith <caleb_smith2@baylor.edu>
2020-12-02 14:00:16 -0700

    format data and train model

--- rnn/text_generation.py
+++ rnn/text_generation.py
@@ -29,5 +29,22 @@ for i in range(data_size - sentence_length):
 num_sentences = len(sentences)
 #print(num_sentences)
 
+# use one-hot character encoding
+
+x = np.zeros((num_sentences, sentence_length, vocab_size), dtype=np.bool)
+y = np.zeros((num_sentences, vocab_size), dtype=np.bool)
+
+for i, sentence in enumerate(sentences):
+    for j, char in enumerate(sentence):
+        x[i, j, char_to_idx[char]] = 1
+    y[i, char_to_idx[next_chars[i]]] = 1
+
+# create model
+model = Sequential()
+model.add(LSTM(256, input_shape=(sentence_length, vocab_size)))
+model.add(Dense(vocab_size))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.fit(x, y, epochs=2, batch_size=256)
 
 
